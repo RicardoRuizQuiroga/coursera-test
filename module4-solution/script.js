ef6dfbd0d28d2bd5920ca6a6b86dd5f9a2b51e6c
Ricardo Ruiz <ricardo.ruiz@oem.com.mx>
2023-03-28 13:01:10 -0600

    java sctript

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Juan";
-names[1]="Pedro";
-names[2]="Robert";
-names[3]="Ricardo";
-names[4]="Antonio";
-names[5]="Enrique";
-names[6]="Raul";
-names[7]="Edwin";
-names[8]="Carlos";
-names[9]="Aurelio";
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
